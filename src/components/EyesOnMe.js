49f49d7431cc8210027db5d2291da2726a1d5654
Adam <ashaw315@gmail.com>
2021-12-16 18:26:47 -0500

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
-// Code EyesOnMe Component Here
+
+import React from "react";
+
+function EyesOnMe() {
+    
+    function handleDumb() {
+        console.log('Good!');
+    }
+
+    function handleDumber () {
+        console.log("Hey! Eyes on me!");
+    }
+    
+
+    return  (
+        <button onFocus={handleDumb} onBlur={handleDumber}>
+        Eyes on me
+        </button>
+    );
+}
+
+export default EyesOnMe;
